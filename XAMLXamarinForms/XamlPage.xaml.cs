d160426880182c36d4d2f1771c2478e4de702f33
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 19:16:37 -0400

    How to use XAML in Xamarin Forms

---
+++ XAMLXamarinForms/XamlPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace XAMLXamarinForms
+{
+	public partial class XamlPage : ContentPage
+	{
+		public XamlPage ()
+		{
+			this.LoadFromXaml (typeof(XamlPage));
+		}
+		async void btn_clicked(object sender, EventArgs args){
+			Button button = (Button)sender;
+			await DisplayAlert("Clicked!",
+				"The button labeled '" + button.Text + "' has been clicked",
+				"OK");
+		}
+	}
+}
+
